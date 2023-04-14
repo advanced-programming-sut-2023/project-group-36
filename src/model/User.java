82f5d2b26c716ab02f77e3e6ab24414436576f21
ali_hashemian <aho.hashemian@gmail.com>
2023-04-14 16:55:21 +0430

    User Class Updated

--- src/model/User.java
+++ src/model/User.java
@@ -12,7 +12,40 @@ public class User {
     private String questionAnswer;
     private ArrayList<Game> games;
 
+    User(String username, String password, String nickname, String email, String slogan, String questionAnswer, int questionNumber){
+
+    }
     public String getUsername() {
         return username;
     }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getSlogan() {
+        return slogan;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void changeProfile(String password, String nickname, String email, String slogan, String questionAnswer, int questionNumber){
+
+    }
+
+    public void startNewGame(Game game){
+        games.add(game);
+    }
+
+    public void removeGame(Game game){
+        games.remove(game);
+    }
+
+
 }
