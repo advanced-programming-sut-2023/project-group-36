59febee25d37b5358b54ae8e40f8e6f359924053
Mohammad <zare.6984@gmail.com>
2023-04-24 16:08:18 +0330

    Gson library for json added

--- src/main/java/project/controller/SaveGame.java
+++ src/main/java/project/controller/SaveGame.java
@@ -1,5 +1,13 @@
 package project.controller;
 
 public class SaveGame {
+    public static String Address;
 
+    public static void setAddress(String address) {
+        Address = address;
+    }
+
+    public static void GameInitialization(){
+
+ }
 }
