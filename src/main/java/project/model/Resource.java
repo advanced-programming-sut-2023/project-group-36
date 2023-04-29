2b9c498040960a21dbe1b4f2a70a42db56bcf49e
ali_hashemian <aho.hashemian@gmail.com>
2023-04-30 01:44:03 +0430

    Add Resource & Resources (classes).

---
+++ src/main/java/project/model/Resource.java
@@ -0,0 +1,23 @@
+package project.model;
+
+public class Resource {
+
+    private final String name;
+    private int amount;
+    public Resource(String name, int amount) {
+        this.amount = amount;
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getCount() {
+        return amount;
+    }
+
+    public void changeCount(int amount){
+        this.amount += amount;
+    }
+}
