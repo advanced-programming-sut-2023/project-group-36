685ac62ed288a3e2354492b15f804df122e0cdb0
Mohammad <zare.6984@gmail.com>
2023-05-05 21:31:10 +0330

    junit-jupiter added 1 testcase written

--- src/main/java/project/model/User.java
+++ src/main/java/project/model/User.java
@@ -1,5 +1,7 @@
 package project.model;
 
+import java.util.Objects;
+
 public class User {
     private String username;
     private String password;
@@ -92,5 +94,18 @@ public class User {
 
     public void addScore(int amount){
         score+=amount;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return questionNumber == user.questionNumber && Objects.equals(username, user.username) && Objects.equals(password, user.password) && Objects.equals(nickname, user.nickname) && Objects.equals(email, user.email) && Objects.equals(slogan, user.slogan) && Objects.equals(questionAnswer, user.questionAnswer);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(username, password, nickname, email, slogan, questionNumber, questionAnswer);
     }
 }
--- src/test/java/project/AppTest.java
+++ src/test/java/project/AppTest.java
@@ -1,38 +1,15 @@
 package project;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.junit.jupiter.api.Assertions;
+import project.model.User;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+public class AppTest {
+    @org.junit.jupiter.api.Test
+    public void RegisterTest () {
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
+        User user1 =new User("1","1","1","1","1","1",1);
+        User user2=new User("1","1","1","1","1","1",1);
+        Assertions.assertEquals(user1,user2);
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
     }
 }
