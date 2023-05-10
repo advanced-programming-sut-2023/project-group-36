984518090bbc63b571583c8b38d69449e7baba0c
Amirmohammad <shzandamr@gmail.com>
2023-05-10 04:38:48 +0430

    change slogan test added

--- src/test/java/project/AppTest.java
+++ src/test/java/project/AppTest.java
@@ -13,41 +13,44 @@ import java.util.regex.Matcher;
 
 public class AppTest {
     @Test
-    public void RegisterTest () {
+    public void RegisterTest() {
 
-        User user1 =new User("1","1","1","1","1","1",1);
-        User user2=new User("1","1","1","1","1","1",1);
-        Assertions.assertEquals(user1,user2);
+        User user1 = new User("1", "1", "1", "1", "1", "1", 1);
+        User user2 = new User("1", "1", "1", "1", "1", "1", 1);
+        Assertions.assertEquals(user1, user2);
     }
+
     @Test
-    public void PasswordTest()throws Exception{
-        User user1 =new User("1","1","1","1","1","1",1);
-        Assertions.assertEquals(SHA_256Format.sha256("1"),SHA_256Format.sha256(user1.getPassword()));
+    public void PasswordTest() throws Exception {
+        User user1 = new User("1", "1", "1", "1", "1", "1", 1);
+        Assertions.assertEquals(SHA_256Format.sha256("1"), SHA_256Format.sha256(user1.getPassword()));
 
     }
+
     @Test
-    public void UserInitilazeTest(){
-        ArrayList<User> users=new ArrayList<>();
-        users.add(new User("1","1","1","1","1","1",1));
-        users.add(new User("2","2","2","2","2","2",2));
-        users.add(new User("3","3","3","3","3","3",3));
-        users.add(new User("4","4","4","4","4","4",4));
+    public void UserInitilazeTest() {
+        ArrayList<User> users = new ArrayList<>();
+        users.add(new User("1", "1", "1", "1", "1", "1", 1));
+        users.add(new User("2", "2", "2", "2", "2", "2", 2));
+        users.add(new User("3", "3", "3", "3", "3", "3", 3));
+        users.add(new User("4", "4", "4", "4", "4", "4", 4));
         SaveAndLoad.saveUsers(users);
         SaveAndLoad.gameInitialization();
-        ArrayList<User> usersToTest= ApplicationManager.getUsers();
-        Assertions.assertEquals(usersToTest,users);
+        ArrayList<User> usersToTest = ApplicationManager.getUsers();
+        Assertions.assertEquals(usersToTest, users);
     }
+
     @Test
     public void TestWithMock() throws NoSuchAlgorithmException {
-    Matcher matcher= Menu.getMatcher("user login -u ahmad -p 123", Commands.LOGIN.getRegex());
-        String out=LoginMenuController.Login(matcher);
-    Assertions.assertEquals("User logged in Successfully!",out);
+        Matcher matcher = Menu.getMatcher("user login -u ahmad -p 123", Commands.LOGIN.getRegex());
+        String out = LoginMenuController.Login(matcher);
+        Assertions.assertEquals("User logged in Successfully!", out);
 
     }
 
     @Test
-    public void changeUsername(){
-        User user = new User("1","1","1","1","1","1",1);
+    public void changeUsername() {
+        User user = new User("1", "1", "1", "1", "1", "1", 1);
         ApplicationManager.setCurrentUser(user);
         String testCommand = "profile change -u amir";
         Matcher matcher = Menu.getMatcher(testCommand, Commands.CHANGE_USERNAME.getRegex());
@@ -59,13 +62,24 @@ public class AppTest {
 
     @Test
     public void changeNickname() {
-        User user = new User("1","1","1","1","1","1",1);
+        User user = new User("1", "1", "1", "1", "1", "1", 1);
         ApplicationManager.setCurrentUser(user);
         String testCommand = "profile change -n amir";
         Matcher matcher = Menu.getMatcher(testCommand, Commands.CHANGE_NICKNAME.getRegex());
         String result = ProfileMenuController.changeNickname(matcher);
 
         Assertions.assertEquals(result, "Nickname changed successfully");
-
-
     }
+
+    @Test
+    public void changeSlogan() {
+        User user = new User("1","1","1","1","1","1",1);
+        ApplicationManager.setCurrentUser(user);
+        String testCommand = "profile change slogan -s amir";
+        Matcher matcher = Menu.getMatcher(testCommand, Commands.CHANGE_SLOGAN.getRegex());
+        String result = ProfileMenuController.changeSlogan(matcher);
+
+        Assertions.assertEquals(result, "Slogan changed successfully");
+        Assertions.assertEquals(user.getSlogan(), "amir");
+    }
+}
