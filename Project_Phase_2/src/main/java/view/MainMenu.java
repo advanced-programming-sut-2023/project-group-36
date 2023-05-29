3d7f4e614f8b57ab51fad955147714db1a3efc9a
mohammad6987 <119624288+mohammad6987@users.noreply.github.com>
2023-05-29 13:17:58 +0330

    new raw project

---
+++ Project_Phase_2/src/main/java/view/MainMenu.java
@@ -0,0 +1,11 @@
+package view;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class MainMenu extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
+}
