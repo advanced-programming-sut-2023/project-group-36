ff13ba4c60dea3717ec04782ec682750eb3fb36e
Mohammad <zare.6984@gmail.com>
2023-05-31 08:29:32 +0330

    update in Errors

--- src/main/java/controller/Erorr.java
+++ src/main/java/controller/Erorr.java
@@ -1,12 +1,25 @@
 package controller;
 
 import javafx.application.Application;
+import javafx.scene.control.Alert;
 import javafx.stage.Stage;
 
 public class Erorr extends Application {
+    private String ErorrName="";
+    private String ErorrDetails="";
+
 
     @Override
     public void start(Stage stage) throws Exception {
+        Alert alert=new Alert(Alert.AlertType.ERROR);
+        alert.setContentText(ErorrDetails);
+        alert.setHeaderText(ErorrName);
+        alert.showAndWait();
 
+    }
+    public void setError(String ErorrName,String ErorrDetails) throws Exception {
+        this.ErorrDetails=ErorrDetails;
+        this.ErorrName=ErorrName;
+        this.start(new Stage());
     }
 }
