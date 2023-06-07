3e604af408a83023000bb88f7c1a165881fae871
Amirmohammad <shzandamr@gmail.com>
2023-06-08 03:05:08 +0330

    taxRate for GovernmentMenu added

--- src/main/java/view/GovernmentMenu.java
+++ src/main/java/view/GovernmentMenu.java
@@ -13,7 +13,9 @@ import javafx.scene.shape.StrokeType;
 import javafx.stage.Stage;
 import model.ApplicationManager;
 
+import java.io.IOException;
 import java.util.Objects;
+import java.util.concurrent.atomic.AtomicReference;
 
 public class GovernmentMenu extends Application {
 
@@ -45,23 +47,23 @@ public class GovernmentMenu extends Application {
             ImageView feedImageView;
 
             if (ApplicationManager.getCurrentGame().getCurrentGovernment().getFeedRate() < 0) {
-                feedImage = new Image(Objects.requireNonNull(getClass().getResource("slightly-frowning-face-apple.png")).openStream());
+                feedImage = (new Image(Objects.requireNonNull(getClass().getResource("slightly-frowning-face-apple.png")).openStream()));
                 feedImageView = new ImageView(feedImage);
                 feedImageView.setFitWidth(20);
                 feedImageView.setFitHeight(20);
-                feedImageView.setStyle("-fx-background-color: red;");
+//                feedImageView.setStyle("-fx-background-color: red;");
             } else if (ApplicationManager.getCurrentGame().getCurrentGovernment().getFeedRate() > 0) {
-                feedImage = new Image(Objects.requireNonNull(getClass().getResource("slightly-smiling-face-apple.png")).openStream());
+                feedImage = (new Image(Objects.requireNonNull(getClass().getResource("slightly-smiling-face-apple.png")).openStream()));
                 feedImageView = new ImageView(feedImage);
                 feedImageView.setFitWidth(20);
                 feedImageView.setFitHeight(20);
-                feedImageView.setStyle("-fx-background-color: green;");
+//                feedImageView.setStyle("-fx-background-color: green;");
             } else {
-                feedImage = new Image(Objects.requireNonNull(getClass().getResource("neutral-face-apple.png")).openStream());
+                feedImage = (new Image(Objects.requireNonNull(getClass().getResource("neutral-face-apple.png")).openStream()));
                 feedImageView = new ImageView(feedImage);
                 feedImageView.setFitWidth(20);
                 feedImageView.setFitHeight(20);
-                feedImageView.setStyle("-fx-background-color: yellow;");
+//                feedImageView.setStyle("-fx-background-color: yellow;");
             }
             feedImageView.setLayoutX(132);
             feedImageView.setLayoutY(328);
@@ -72,7 +74,9 @@ public class GovernmentMenu extends Application {
                 int newFeedRate = Integer.parseInt(feedButton.getText()) + 1;
                 if (newFeedRate > 2) {
                     newFeedRate = newFeedRate % 2 - 3;
+                    //set image
                 }
+                //set image
                 feedButton.setText(String.valueOf(newFeedRate));
                 ApplicationManager.getCurrentGame().getCurrentGovernment().setFeedRate(newFeedRate);
             });
@@ -84,6 +88,52 @@ public class GovernmentMenu extends Application {
             pane.getChildren().add(feedButton);
         }
 
+        {
+            Label taxLabel = new Label();
+            taxLabel.setLayoutX(323);
+            taxLabel.setLayoutY(328);
+            taxLabel.setText("Tax");
+            pane.getChildren().add(taxLabel);
+
+            Image taxImage;
+            ImageView taxImageView;
+
+            if (ApplicationManager.getCurrentGame().getCurrentGovernment().getTaxRate() < 0) {
+                taxImage = new Image(Objects.requireNonNull(getClass().getResource("slightly-smiling-face-apple.png")).openStream());
+                taxImageView = new ImageView(taxImage);
+                taxImageView.setFitWidth(20);
+                taxImageView.setFitHeight(20);
+//                taxImageView.setStyle("-fx-background-color: green;");
+            } else if (ApplicationManager.getCurrentGame().getCurrentGovernment().getTaxRate() > 0) {
+                taxImage = new Image(Objects.requireNonNull(getClass().getResource("slightly-smiling-face-apple.png")).openStream());
+                taxImageView = new ImageView(taxImage);
+                taxImageView.setFitWidth(20);
+                taxImageView.setFitHeight(20);
+            } else {
+                taxImage = new Image(Objects.requireNonNull(getClass().getResource("slightly-smiling-face-apple.png")).openStream());
+                taxImageView = new ImageView(taxImage);
+                taxImageView.setFitWidth(20);
+                taxImageView.setFitHeight(20);
+            }
+            taxImageView.setLayoutX(301);
+            taxImageView.setLayoutY(328);
+            pane.getChildren().add(taxImageView);
+
+            Button taxButton = new Button();
+            taxButton.setText("1");
+            taxButton.setOnMouseClicked(e -> {
+                int newTaxRate = Integer.parseInt(taxButton.getText())+1;
+                if (newTaxRate > 8) {
+                    newTaxRate = newTaxRate % 8 - 4;
+                    //set image
+                }
+                //set image
+                taxButton.setText(String.valueOf(newTaxRate));
+            });
+            taxButton.setLayoutX(274);
+            taxButton.setLayoutY(324);
+            pane.getChildren().add(taxButton);
+        }
 
 
 
