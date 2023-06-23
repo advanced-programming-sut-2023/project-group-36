3849d669413bdeed302a9a4197c8d439c9b688c1
Mohammad <zare.6984@gmail.com>
2023-06-23 23:05:10 +0330

    update in profile menu

--- src/main/java/view/ProfileMenu.java
+++ src/main/java/view/ProfileMenu.java
@@ -1,11 +1,21 @@
 package view;
 
+import javafx.animation.Timeline;
 import javafx.application.Application;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class ProfileMenu extends Application {
+    Timeline timeline;
     @Override
     public void start(Stage stage) throws Exception {
+        Pane pane=new Pane();
+        VBox info=new VBox();
+        Label username=new Label("username = "+"");//incomplete
+        Label nickname=new Label("nickname = "+"");
+
 
     }
 }
