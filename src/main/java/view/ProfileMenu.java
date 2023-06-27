2da3eab283f7ca23b8b81d906b2b5a4ea2ba6557
Mohammad <zare.6984@gmail.com>
2023-06-28 01:00:36 +0330

    update in profile menu (70%)

--- src/main/java/view/ProfileMenu.java
+++ src/main/java/view/ProfileMenu.java
@@ -7,6 +7,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.input.ClipboardContent;
 import javafx.scene.input.DragEvent;
@@ -23,6 +24,7 @@ import javafx.scene.paint.ImagePattern;
 import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 import model.ApplicationManager;
+import model.User;
 
 public class ProfileMenu extends Application {
     Timeline timeline;
@@ -51,6 +53,7 @@ public class ProfileMenu extends Application {
         info.getChildren().add(CurrentNickname);
         info.getChildren().add(CurrnetEmail);
         info.getChildren().add(CurrentSlogan);*/
+        //changing info
         info.setBorder(new Border(new BorderStroke(Color.BLUEVIOLET, BorderStrokeStyle.SOLID,new CornerRadii(5),new BorderWidths(3))));
         pane.getChildren().add(info);
         VBox ChangeInfo=new VBox();
@@ -59,14 +62,70 @@ public class ProfileMenu extends Application {
         ChangeInfo.setLayoutX(202);
         ChangeInfo.setMinWidth(880);
         ChangeInfo.setMinHeight(400);
+        ChangeInfo.setSpacing(25);
         ChangeInfo.setAlignment(Pos.TOP_CENTER);
+        ChangeInfo.getChildren().add(new Label("Change Info\n\n"));
+        //username
         HBox changeUsername=new HBox();
+        changeUsername.setBorder(new Border(new BorderStroke(Color.BLUEVIOLET, BorderStrokeStyle.SOLID,new CornerRadii(5),new BorderWidths(3))));
         changeUsername.setAlignment(Pos.TOP_CENTER);
+        changeUsername.setMinHeight(40);
         Label changeUser=new Label("new Username :");
         TextField changeNameField=new TextField("");
+        changeNameField.setMinWidth(200);
         Label UsernameCheck=new Label("");
+        changeUsername.setSpacing(25);
         changeUsername.getChildren().addAll(changeUser,changeNameField,UsernameCheck);
         ChangeInfo.getChildren().add(changeUsername);
+        //password
+        HBox passwordBox=new HBox();
+        passwordBox.setBorder(new Border(new BorderStroke(Color.BLUEVIOLET, BorderStrokeStyle.SOLID,new CornerRadii(5),new BorderWidths(3))));
+        passwordBox.setAlignment(Pos.TOP_CENTER);
+        passwordBox.setSpacing(25);
+        Label passwordLabel =new Label("Password :");
+        Label currPassCheck=new Label("");
+        Label newPassCheck=new Label("");
+        TextField currentPassword=new TextField();
+        currentPassword.setPromptText("current password");
+        currentPassword.setMinWidth(200);
+        TextField newPassword=new TextField();
+        newPassword.setPromptText("new password");
+        newPassword.setMinWidth(200);
+        VBox passwords=new VBox(new HBox(currentPassword,currPassCheck),new HBox(newPassword,newPassCheck));
+        passwords.setSpacing(25);
+        passwordBox.getChildren().addAll(passwordLabel,passwords);
+        ChangeInfo.getChildren().add(passwordBox);
+        //nickname
+        HBox nicknameBox=new HBox();
+        nicknameBox.setSpacing(25);
+        nicknameBox.setAlignment(Pos.TOP_CENTER);
+        nicknameBox.setBorder(new Border(new BorderStroke(Color.BLUEVIOLET, BorderStrokeStyle.SOLID,new CornerRadii(12),new BorderWidths(1))));
+        Label nicknameLabel=new Label("nickname : ");
+        TextField newNickname=new TextField();
+        Label NicknameCheck=new Label("");
+        newNickname.setPromptText("new nickname");
+        newNickname.setMinWidth(200);
+        nicknameBox.getChildren().addAll(nicknameLabel,newNickname,NicknameCheck);
+        ChangeInfo.getChildren().add(nicknameBox);
+        //email
+        HBox emailBox=new HBox();
+        emailBox.setSpacing(37);
+        emailBox.setAlignment(Pos.TOP_CENTER);
+        emailBox.setBorder(new Border(new BorderStroke(Color.BLUEVIOLET, BorderStrokeStyle.SOLID,new CornerRadii(12),new BorderWidths(1))));
+        Label emailLabel=new Label("email :   ");
+        TextField newemail=new TextField();
+        Label EmailCheck=new Label("");
+        newemail.setPromptText("new email");
+        newemail.setMinWidth(200);
+        emailBox.getChildren().addAll(emailLabel,newemail,EmailCheck);
+        ChangeInfo.getChildren().add(emailBox);
+        //slogan
+        HBox sloganBox=new HBox();
+
+
+        //scoreboard
+        TableView<User> scoreboard=new TableView<User>();
+
         pane.getChildren().add(ChangeInfo);
         stage.setScene(new Scene(pane));
         stage.show();
