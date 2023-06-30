3c322e8a5b68429f5209159a2f2009756ef8b079
Mohammad <zare.6984@gmail.com>
2023-06-30 12:36:39 +0330

    adding main menu background and fonts

--- src/main/java/view/MainMenu.java
+++ src/main/java/view/MainMenu.java
@@ -6,6 +6,7 @@ import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
@@ -17,6 +18,11 @@ public class MainMenu extends Application {
         Pane pane=new Pane();
         pane.setMinWidth(800);
         pane.setMinHeight(600);
+        pane.setStyle("-fx-font-family: Arial; -fx-font-size: 18px; -fx-text-fill: #ffffff");
+        BackgroundImage myBI1= new BackgroundImage(new Image(LoginMenu.class.getResource("/images/34192.jpg").toString(),800,600,false,true),
+                BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT,
+                BackgroundSize.DEFAULT);
+        pane.setBackground(new Background(myBI1));
         VBox vBox=new VBox();
         Button createGame=new Button("Create New Game");
         Button GamesList=new Button("Games List");
@@ -27,13 +33,14 @@ public class MainMenu extends Application {
         vBox.setAlignment(Pos.TOP_CENTER);
         vBox.setPadding(new Insets(5,10,5,10));
         vBox.setSpacing(25);
-        vBox.setLayoutX(50);
-        vBox.setLayoutY(25);
+        vBox.setLayoutX(100);
+        vBox.setLayoutY(50);
         for(Node node:vBox.getChildren()){
             if(node instanceof Button){
+                ((Button) node).setTextFill(Color.WHITE);
                 ((Button) node).setBackground(Background.fill(Color.TRANSPARENT));
                 ((Button) node).setBorder(new Border(new BorderStroke(Color.BLACK, BorderStrokeStyle.SOLID,new CornerRadii(5),new BorderWidths(3))));
-                ((Button) node).setMinWidth(120);
+                ((Button) node).setMinWidth(200);
                 ((Button) node).setMinHeight(25);
             }
         }
